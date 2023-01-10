4aac05024cb82007c36cff8067f26bf97325e762
Noot <brycechen987@gmail.com>
2023-01-09 23:30:09 -0500

    changes

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,10 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.Events;
-
-using System;
+using UnityEngine.SceneManagement;
 [System.Serializable]
 
 public static class Constants
